95e1b889066554c03f1f6173eccada3eb9b1241b
Igor <igordearaujoborges67@gmail.com>
2026-08-28 20:57:03 -0300

    push 2

--- Assets/controllPadldle.cs
+++ Assets/controllPadldle.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class controllPadldle : MonoBehaviour
 {
+
+    public KeyCode moveRight = KeyCode.D;      // Move a raquete para cima
+    public KeyCode moveLeft = KeyCode.A;    // Move a raquete para baixo
+    public float speed = 10.0f;             // Define a velocidade da raquete
+    public float boundX = 2.25f;            // Define os limites em Y
+    private Rigidbody2D rb2d;  
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         rb2d = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
+        var vel = rb2d.linearVelocity;                // Acessa a velocidade da raquete
+        if (Input.GetKey(moveRight)) {             // Velocidade da Raquete para ir para cima
+            vel.x = speed;
+        }
+        else if (Input.GetKey(moveLeft)) {      // Velocidade da Raquete para ir para cima
+            vel.x = -speed;                    
+        }
+        else {
+            vel.x = 0;                          // Velociade para manter a raquete parada
+        }
+        rb2d.linearVelocity = vel;                    // Atualizada a velocidade da raquete
+
+        var pos = transform.position;           // Acessa a Posição da raquete
+        if (pos.x > boundX) {                  
+            pos.x = boundX;                     // Corrige a posicao da raquete caso ele ultrapasse o limite superior
+        }
+        else if (pos.x < -boundX) {
+            pos.x = -boundX;                    // Corrige a posicao da raquete caso ele ultrapasse o limite inferior
+        }
+        transform.position = pos;               // Atualiza a posição da raquete
+    
     }
+
 }
